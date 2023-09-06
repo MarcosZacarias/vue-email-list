c27c972aee554beace8ec7f7b99a6133048c5c0b
MarcosZacarias <marcoszacariasdaniel@gmail.com>
2023-09-06 16:07:41 +0200

    add: generate 10 mails

--- js/main.js
+++ js/main.js
@@ -4,18 +4,23 @@ createApp({
   data() {
     return {
       message: "Hello World",
+      listMails: [],
     };
   },
 
   methods: {
     generateMail() {
-      axios
-        .get("https://flynn.boolean.careers/exercises/api/random/mail")
-        .then((response) => {
-          const result = response.data.response;
-          console.log(result);
-          this.message = result;
-        });
+      for (let i = 0; i < 10; i++) {
+        axios
+          .get("https://flynn.boolean.careers/exercises/api/random/mail")
+          .then((response) => {
+            const result = response.data.response;
+            // console.log(result);
+            this.listMails.push(result);
+          });
+      }
+
+      console.table(this.listMails);
     },
   },
 
