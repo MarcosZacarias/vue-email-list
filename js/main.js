846751bcbcbc16ddda38a89745a1513ffecb6679
MarcosZacarias <marcoszacariasdaniel@gmail.com>
2023-09-06 15:39:04 +0200

    add: function generate mail with axios

--- js/main.js
+++ js/main.js
@@ -6,4 +6,20 @@ createApp({
       message: "Hello World",
     };
   },
+
+  methods: {
+    generateMail() {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((response) => {
+          const result = response.data.response;
+          console.log(result);
+          this.message = result;
+        });
+    },
+  },
+
+  mounted() {
+    this.generateMail();
+  },
 }).mount("#app");
